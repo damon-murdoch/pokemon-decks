db6b0753d7aac5969141b3bbf359a34f328585d8
Damon Murdoch <damonmurdoch1@gmail.com>
2025-04-17 00:41:46 +1000

    Added standard decks
    Added standard decks

--- src/format.js
+++ src/format.js
@@ -1,16 +1,20 @@
+// getLogoPath(path: String): String
+// Given a logo name, returns the file
+// path of the logo.
+function getLogoPath(path) {
+  // If no path
+  if (path === '')
+    return null; // No sprite
+
+  // Retrn the path to the 30px sprite for the format
+  return "img/symbol/30px-SetSymbol" + path + ".webp";
+}
+
 // getFormatLogo(start: String, end: String): Object
 // Given a start and end format, gets the
 // path of the start and end sprites and
 // returns them to the calling process.
 function getFormatLogos(start, end) {
-  // getLogoPath(path: String): String
-  // Given a logo name, returns the file
-  // path of the logo.
-  function getLogoPath(path) {
-    // Retrn the path to the 30px sprite for the format
-    return "img/symbol/30px-SetSymbol" + path + ".webp";
-  }
-
   // Return the start and end sprite paths
   return {
     // Format Starting Sprite
@@ -39,24 +43,26 @@ function getFormatTitle(format) {
       data.meta.end // Ending set logo
     );
 
+    // Container Placeholder
+    let content = "";
+    
+    // Add start sprite
+    if (sprites.start) {
+      content += `<img style='width:30px; height:30px;' src='${sprites.start}'></img>`;
+    }
+
+    // Add format link
+    content += `<a href='${getPageLink(format)}' class='text-light px-2'>${format}</a>`;
+
+    // Add end sprite
+    if (sprites.end) {
+      content += `<img style='width:30px; height:30px;' src='${sprites.end}'></img>`;
+    }
+
     // Return a container for the format title including both starting
     // and ending sprites, as well as the format title. When clicked,
     // the format name links to the format page.
-    return (
-      "<div class='d-flex justify-content-center'>" +
-      "<img style='width:30px; height:30px;' src='" +
-      sprites.start +
-      "'>" +
-      "</img><a href='" +
-      getPageLink(format) +
-      "' class='text-light px-2'>" +
-      format +
-      "</a>" +
-      "<img style='width:30px; height:30px;' src='" +
-      sprites.end +
-      "'>" +
-      "</img></div>"
-    );
+    return `<div class='d-flex justify-content-center'>${content}</div>`;
   }
 
   // Return null by default
